7470ca5d604ce7241baf114dfea2a9707c7d465a
Ivan Ivashov <IvashovIA@ya.ru>
2026-03-07 21:40:47 +0300

    Добавлен файл с константами

---
+++ src/Constants/constants.js
@@ -0,0 +1,33 @@
+import telegram from "../components/Footer/img/Telegram.svg";
+import youtube from "../components/Footer/img/Youtube.svg";
+import tiktok from "../components/Footer/img/tiktok.svg";
+import figma from "../components/Footer/img/Figma.svg";
+import github from "../components/Footer/img/Github.svg";
+
+export const NAVIGATION_ITEMS = [
+  "База вопросов",
+  "Тренажер",
+  "Материалы",
+  "Навыки (hh)",
+];
+
+export const SPECIALIZATION = [
+  "UI/UX designe",
+  "Frontend developer",
+  "Backed developer",
+  "Fullstack",
+  "Figma",
+];
+
+export const SKILLS = ["Figma", "CSS", "React", "HTML"];
+export const LEVEL = ["1-3", "4-6", "7-8", "9-10"];
+export const RATING = ["1", "2", "3", "4", "5"];
+export const STATUS = ["Изученные", "Неизученные", "Все"];
+
+export const SOCIAL_ICON = [
+  { id: 1, title: "Figma", iconSrc: figma },
+  { id: 2, title: "Telegram", iconSrc: telegram },
+  { id: 3, title: "Youtube", iconSrc: youtube },
+  { id: 4, title: "Tiktok", iconSrc: tiktok },
+  { id: 5, title: "GitHub", iconSrc: github },
+];
