e96099b983b28d5dc39c60528d20ebab3c1bdd8f
Ivan Ivashov <IvashovIA@ya.ru>
2026-03-08 15:02:09 +0300

    обновление компонента QuestionItem

--- src/components/QuestionItem/QuestionItem.jsx
+++ src/components/QuestionItem/QuestionItem.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./questionItem.module.css";
 
 import circle from "../QuestionItem/img/circle.svg";
@@ -6,39 +6,19 @@ import arrowDown from "../QuestionItem/img/arrowDown.svg";
 import close from "../QuestionItem/img/close.svg";
 import answerImg from "../QuestionItem/img/answerImg.jpg";
 
-function QuestionItem() {
+function QuestionItem({ question, img }) {
   const [isOpen, setIsOpen] = useState(false);
-  const [getQuestions, setGetQuestions] = useState([]);
 
   function handleClick() {
     setIsOpen((prev) => !prev);
   }
-
-  useEffect(() => {
-    async function getData() {
-      try {
-        const res = await fetch(
-          "https://api.yeatwork.ru/questions/public-questions?page=1&limit=10",
-        );
-        if (!res.ok) {
-          throw new Error(`Ошибка HTTP: ${res.status}`);
-        }
-        const data = await res.json();
-        setGetQuestions(data);
-        console.log(data);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    getData();
-  }, []);
 
   return (
     <div className={styles.question__accordion}>
       <div className={styles.question__title} onClick={handleClick}>
         <h2>
           <img src={circle} alt="circle" />
-          Что такое Virtual DOM и как он работает?
+          {question.title}
         </h2>
         <button className={styles.accordion__btn}>
           <img
@@ -53,10 +33,10 @@ function QuestionItem() {
           <div className={styles.question__info}>
             <div className={styles.info__left}>
               <p>
-                Рейтинг: <span>4</span>
+                Рейтинг: <span>{question.rate}</span>
               </p>
               <p>
-                Сложность: <span>10</span>
+                Сложность: <span>{question.complexity}</span>
               </p>
             </div>
             <button className={styles.edit__btn}>
@@ -65,21 +45,18 @@ function QuestionItem() {
           </div>
 
           <div className={styles.example}>
-            <img
-              className={styles.example__img}
-              src={answerImg}
-              alt="example"
-            />
+            {img && (
+              <img
+                className={styles.example__img}
+                src={question.imageSrc}
+                alt="example"
+              />
+            )}
+
             <p className={styles.desc}>
-              Virtual DOM (виртуальный DOM) — это программная концепция,
-              используемая в разработке веб-приложений для повышения
-              эффективности обновлений интерфейса. Это представление реального
-              DOM (структуры документа, отображаемого в браузере) в памяти,
-              которое позволяет оптимизировать изменения, минимизируя
-              взаимодействие с реальным DOM, что ускоряет рендеринг и обновление
-              страниц. При изменении данных приложения Virtual DOM сравнивает
-              новое состояние с предыдущим и обновляет только те части реального
-              DOM, которые изменились, вместо перерисовки всего документа.
+              {question.longAnswer ||
+                question.description ||
+                "Описание отсутствует"}
             </p>
           </div>
         </div>
